734436cc37b3c03515e479020d94510ce116b736
rafailpapastamou <raphaelpapast@gmail.com>
2023-12-02 20:41:45 +0200

    - Created the contact page - Updated some issues with the navbar

---
+++ javascript-files/contact-form.js
@@ -0,0 +1,105 @@
+document.addEventListener("DOMContentLoaded", function () {
+    var contactForm = document.getElementById("contactForm");
+    
+    // Add event listener to the form
+    contactForm.addEventListener("submit", function (event) {
+      if (!validateForm()) {
+        // If form validation fails, prevent form submission
+        event.preventDefault();
+      }
+    });
+  
+    // Add event listener to page unload
+    window.addEventListener("unload", function () {
+
+        // Clear the form fields
+        contactForm.elements["name"].value = "";
+        contactForm.elements["email"].value = "";
+        contactForm.elements["text"].value = "";
+    });
+
+  
+    // Add event listener to page refresh
+    window.addEventListener("beforeunload", function (event) {
+      // Check if the form has any input values
+      if (
+        contactForm.elements["name"].value ||
+        contactForm.elements["email"].value ||
+        contactForm.elements["text"].value
+      ) {
+        // Prompt the user before leaving the page
+        var confirmation = window.confirm(
+          "Refreshing the page will delete the form data. Are you sure you want to proceed?"
+        );
+  
+        if (!confirmation) {
+          // Cancel the page refresh if the user clicks "Cancel" in the confirmation dialog
+          event.preventDefault();
+        }
+      }
+    });
+  });
+
+// Function to validate the contact form
+function validateForm(event) {
+    event.preventDefault(); // Prevent the form from submitting by default
+
+    var name = document.forms["contactForm"]["name"].value;
+    var email = document.forms["contactForm"]["email"].value;
+    var message = document.forms["contactForm"]["text"].value;
+
+    // Reset styles to default
+    resetStyles(document.forms["contactForm"]["name"], "nameError");
+    resetStyles(document.forms["contactForm"]["email"], "emailError");
+    resetStyles(document.forms["contactForm"]["text"], "textError");
+
+    // Check if name is empty or contains numbers
+    if (name == "") {
+        showError(document.forms["contactForm"]["name"], "Εισαγάγετε ονοματεπώνυμο.", "nameError");
+        return false;
+    } else if (!/^[a-zA-Zα-ωΑ-Ω\s]+$/.test(name)) {
+        showError(document.forms["contactForm"]["name"], "Εισάγετε έγκυρο ονοματεπώνυμο.", "nameError");
+        return false;    
+    } else if (name.length < 5) {
+        showError(document.forms["contactForm"]["name"], "Το ονοματεπώνυμο πρέπει να έχει τουλάχιστον 5 χαρακτήρες.", "nameError");
+        return false;
+    }
+
+    // Check if email is a valid email address
+    var emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    if (email == "") {
+        showError(document.forms["contactForm"]["email"], "Εισαγάγετε μια διεύθυνση email.", "emailError");
+        return false;
+    } else if(!emailRegex.test(email)){
+        showError(document.forms["contactForm"]["email"], "Εισαγάγετε μια έγκυρη διεύθυνση email.", "emailError");
+        return false;
+    }
+
+    // Check if message is empty
+    if (message == "") {
+        showError(document.forms["contactForm"]["text"], "Συμπληρώστε το πεδίο μηνύματος.", "textError");
+        return false;
+    }
+
+    // Clear the form fields
+    document.forms["contactForm"]["name"].value = "";
+    document.forms["contactForm"]["email"].value = "";
+    document.forms["contactForm"]["text"].value = "";
+
+    // Show a success message
+    alert("Το μήνυμά σας στάλθηκε με επιτυχία. Θα σας απαντήσουμε το συντομότερο δυνατό.");
+
+    return true;
+}
+
+// Function to reset styles to default
+function resetStyles(element, errorId) {
+    element.style.borderColor = "#94c970";
+    document.getElementById(errorId).innerHTML = '';
+}
+
+// Function to show error message and update styles
+function showError(element, errorMessage, errorId) {
+    element.style.borderColor = "red";
+    document.getElementById(errorId).innerHTML = errorMessage;
+}
